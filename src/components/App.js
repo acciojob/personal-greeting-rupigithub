ce4821d614b50f856cd04058998158e18c705ce9
rupigithub <singhrupika4@gmail.com>
2024-07-26 00:58:52 +0530

    app commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const handleInput = (e)=>{
+    setInput(e.target.value);
+    e.preventDefault();
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Enter your Name:</h1>
+        <form>
+          <input type="text" value={input} onChange={handleInput}></input>
+        </form>
+        <p>{`Hello ${input}!`}</p>
     </div>
   )
 }
